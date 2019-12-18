273dc071ca70b99cc2114551cd27b345a5f82f89
Jefte Santos <jeftenunes@hotmail.com>
2019-12-17 20:53:44 -0500

    rxjs studies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import logo from './logo.svg';
+import DblClickButton from './components/dbl-click-button';
 
 function App() {
   return (
     <div className="App">
       <DblClickButton labelText='Click me!'/>
-      {/* <StreamSubscribe /> */} 
+      {/* <StreamSubscribe /> */}
     </div>
   );
 }
